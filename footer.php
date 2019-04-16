70383e1aaaea2f83bb7fde657c1044800803e363
keqpup232 <keqpup232@gmail.com>
2019-04-16 18:42:45 +0300

    изначальный футер

---
+++ footer.php
@@ -0,0 +1,16 @@
+	<footer id="footer">
+		<div class="container">
+			<div class="footer-holder">
+                <div class="row">
+                    <div class="col-md-6 text-right">
+                    </div>
+                </div>
+			</div>
+		</div>
+	</footer>
+</div>
+	
+<?php wp_footer(); ?>
+
+</body>
+</html>
