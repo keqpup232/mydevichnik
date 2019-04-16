fc8f3161eec91a2db6ea5210b7484ebb6d1990dc
keqpup232 <keqpup232@gmail.com>
2019-04-16 18:45:53 +0300

    добавил обратную связь ссылки на соц сети

--- footer.php
+++ footer.php
@@ -2,7 +2,18 @@
 		<div class="container">
 			<div class="footer-holder">
                 <div class="row">
+                	<div class="col-md-6">
+                    	<h3>Сайт разработан - <a href="https://vk.com/keqpup232">Гаврюшин Иван</a></h3>
+                        <p>© 2019 - <?php echo date("y");?> гг. mydevichnik </p>
+						<br>
+                    </div>
                     <div class="col-md-6 text-right">
+                    	<ul class="social">
+                        	<li><a href="https://www.facebook.com/profile.php?id=100001787401407"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://vk.com/kaaaaaasik"><i class="fab fa-vk"></i></a></li>
+                            <li><a href="https://t.me/keqpup232"><i class="fab fa-telegram-plane"></i></a></li>
+							<li><a href="https://www.instagram.com/iollik/"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
                     </div>
                 </div>
 			</div>
