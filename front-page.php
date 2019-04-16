3a9c696b1c68800e556f16096f7a9124985068bf
keqpup232 <keqpup232@gmail.com>
2019-04-16 18:58:46 +0300

    главная уникальная страничка, будут правки скорее по тексту и картинкам

---
+++ front-page.php
@@ -0,0 +1,169 @@
+<?php get_header();?>
+	<section class="main">
+		<div class="container">
+			<div id="cta">
+				<a href="javascript:" class="btn btn-primary rounded">Заказать обратный звонок</a>
+				<p>Оставьте обратный звонок. Мы обязательно с вами свяжемся и организуем лучший девичник.</p>
+			</div>
+			<div class="row">
+				<div class="text-box col-md-offset-1 col-md-10">
+					<h2>О Нас</h2>
+					<p>Мы молодая команда девочек. Готовые предоставить Вам свои услуги по организации и проведения девичника на Ваше тематику.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<section class="area">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5">
+					<ul class="visual-list">
+						<li>
+							<div class="img-holder">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/graph-04.svg" width="110'; ?>" alt="девичник">
+							</div>
+							<div class="text-holder">
+								<h3>Фотосъёмка</h3>
+								<p>Подберем вам лучшего фотографа на Ваш девичник.  девичник девичник девичник.девичник девичник девичник.девичник девичник девичник</p>
+							</div>
+						</li>
+						<li>
+							<div class="img-holder">
+								<img class="pull-left" src="<?php echo get_template_directory_uri() . '/assets/images/graph-03.svg'; ?>" width="90" alt="девичник">
+							</div>
+							<div class="text-holder">
+								<h3>Видеосъёмка</h3>
+								<p>Подберем вам лучшего видеооператора на Ваш девичник. девичник девичник девичник.девичник девичник девичник.девичник девичник девичник</p>
+							</div>
+						</li>
+						<li>
+							<div class="img-holder">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/graph-02.svg'; ?>" height="84" alt="девичник">
+							</div>
+							<div class="text-holder">
+								<h3>Облако шаров</h3>
+								<p>При любом заказе стандартного кейса облако шаров в подарок на Ваш девичник.девичник девичник девичник.девичник девичник девичник.девичник девичник девичник </p>
+							</div>
+						</li>
+						<li>
+							<div class="img-holder">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/graph-01.svg'; ?>" height="71" alt="девичник">
+							</div>
+							<div class="text-holder">
+								<h3>Уникальность</h3>
+								<p>Каждый наш девичник уникален для каждой невесты. Все заботы берём на себя, Вы только наслаждаетесь моментом! девичник девичник девичник.девичник девичник девичник</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-7">
+					<div class="slide-holder">
+						<div class="img-slide scroll-trigger"><img src="<?php echo get_template_directory_uri() . '/assets/images/img-01.png'; ?>" height="624" width="684" alt="девичник"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="visual-container">
+		<div class="visual-area">
+			<div class="container">
+				<h2>Возможные варианты проведения девичника</h2>
+				<div class="pricing-tables">
+					<div class="plan">
+		                <div class="head">
+		                    <h3>Дома</h3>
+		                </div>
+		                <div class="price">
+		                    <span class="price-main"><span class="symbol">от</span>4</span>
+		                    <span class="price-additional">тыс.руб.</span>
+		                </div>
+		                <ul class="item-list">
+		                    <li>Пижамная</li>
+		                </ul>
+		                <button type="button" class="btn btn-default rounded">заказать</button>
+		            </div>
+		            <div class="plan">
+		                <div class="head">
+		                    <h3>На воздухе</h3> </div>
+		                <div class="price">
+		                    <span class="price-main"><span class="symbol">от</span>12</span>
+		                    <span class="price-additional">тыс.руб.</span>
+		                </div>
+		                    <ul class="item-list">
+		                       <li>Пикник</li>
+		                       <li>Прогулки</li>
+		                    </ul>
+		                <button type="button" class="btn btn-default rounded">заказать</button>
+		            </div>
+		            <div class="plan">
+		                <div class="head">
+		                    <h3>В Клубе</h3> </div>
+		                <div class="price">
+		                    <span class="price-main"><span class="symbol">от</span>20</span>
+		                    <span class="price-additional">тыс.руб.</span>
+		                </div>
+		                    <ul class="item-list">
+		                        <li>Развлекательный</li>
+		                        <li>Ночной</li>
+		                    </ul>
+		                <button type="button" class="btn btn-default rounded">заказать</button>
+		            </div>
+		            <div class="plan">
+		                <div class="head">
+		                    <h3>Индивидуальный</h3> </div>
+		                <div class="price">
+		                    <span class="price-main"><span class="symbol">от</span>25</span>
+		                    <span class="price-additional">тыс.руб.</span>
+		                </div>
+		                <ul class="item-list">
+		                    <li>Ваши капризы</li>
+		                    <li>Связь 24/7</li>
+		                </ul>
+		                <button type="button" class="btn btn-default rounded">заказать</button>
+		            </div>
+				</div>
+ 			</div>
+		</div>
+	</section>
+		<section class="visual-container">
+		<div class="visual-area">
+			<div class="container">
+				<h2>Отзывы наших клиентов</h2>
+				<ul class="testimonials">
+					<li>
+						<div class="img-holder"><a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/images/logo-1.png'; ?>" height="84" width="124" alt="девичник"></a></div>
+						<p><em>девичник</em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/images/logo-2.png'; ?>" height="90" width="148" alt="девичник"></a></div>
+						<p><em>девичник</em></p>
+					</li>
+					<li>
+						<div class="img-holder"><a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/images/logo-3.png'; ?>" height="100" width="100" alt="девичник"></a></div>
+						<p><em>девичник</em></p>
+					</li>
+					
+					<li>
+						<div class="img-holder"><a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/images/logo-4.png'; ?>" height="50" width="108" alt="девичник"></a></div>
+						<p><em>девичник</em></p>
+					</li>
+				</ul>
+			</div>
+		</div>
+		</section>  
+	
+	<section class="area">
+		<div class="container">
+			<div class="subscribe">
+				<h3>Узнайте сценарий и стоимость Вашего мероприятия</h3>
+				<form class="form-inline">
+					<button type="submit" class="btn btn-primary rounded">subscribe</button>
+					<div class="form-group">
+						<input type="email" class="form-control rounded" id="exampleInputEmail2" placeholder="Email...">
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
+<?php get_footer(); ?>
